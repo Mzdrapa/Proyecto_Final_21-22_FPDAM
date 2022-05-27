3d942982a7d4ace0addc8a62e0d5d6bfd82e7dd0
Pablo Ubeda <drakoyuzz@gmail.com>
2022-05-27 18:59:49 +0200

    Toda la funcionalidad del dado terminada

--- app/src/main/java/com/ubedaPablo/proyecto/Dice.kt
+++ app/src/main/java/com/ubedaPablo/proyecto/Dice.kt
@@ -1,13 +1,64 @@
 package com.ubedaPablo.proyecto
 
-import android.graphics.drawable.Drawable
+import android.content.Context
+import android.os.Handler
+import android.os.Looper
+import android.widget.ImageView
+import android.widget.TextView
+import java.util.*
+import kotlin.concurrent.scheduleAtFixedRate
+import kotlin.math.roundToInt
 import kotlin.random.Random
 
-data class Dice(var tipo: String = "D20") {
-    lateinit var drawable: Drawable
-    var num: Int = 0
-    fun lanzarDado() {
-        num = Random.nextInt(tipo.removePrefix("D").toInt())
+data class Dice(val text: TextView, val img: ImageView, val context: Context) {
+    var image: Int = R.drawable.ic_d20
+    var maxValueOfDice: Int = 20
 
+    fun diceClicked(type: Int) {
+        when (type) {
+            20 -> {
+                maxValueOfDice = 20
+                image = R.drawable.ic_d20
+            }
+            12 -> {
+                maxValueOfDice = 12
+                image = R.drawable.ic_d12
+            }
+            10 -> {
+                maxValueOfDice = 10
+                image = R.drawable.ic_d10
+            }
+            8 -> {
+                maxValueOfDice = 8
+                image = R.drawable.ic_d8
+            }
+            6 -> {
+                maxValueOfDice = 6
+                image = R.drawable.ic_d6
+            }
+            4 -> {
+                maxValueOfDice = 4
+                image = R.drawable.ic_d4
+            }
+        }
+        drawImage()
+        generateRandomNumbers()
+    }
+
+    private fun generateRandomNumbers() {
+        var numbersGenerated = 0
+        val numbersTried = Random.nextInt(maxValueOfDice, maxValueOfDice.times(1.5).roundToInt())
+        Timer().scheduleAtFixedRate(0, 100) {
+            Handler(Looper.getMainLooper()).post {
+                val num = Random.nextInt(1, maxValueOfDice + 1).toString()
+                text.text = num
+                if (++numbersGenerated >= numbersTried) cancel()
+
+            }
+        }
+    }
+
+    private fun drawImage() {
+        img.setImageResource(image)
     }
 }
--- app/src/main/java/com/ubedaPablo/proyecto/ui/dice/DiceFragment.kt
+++ app/src/main/java/com/ubedaPablo/proyecto/ui/dice/DiceFragment.kt
@@ -1,31 +1,25 @@
 package com.ubedaPablo.proyecto.ui.dice
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.ubedaPablo.proyecto.Dice
-import com.ubedaPablo.proyecto.R
 import com.ubedaPablo.proyecto.databinding.FragmentDiceBinding
-import java.util.*
-import kotlin.concurrent.scheduleAtFixedRate
-import kotlin.random.Random
 
 class DiceFragment : Fragment(), View.OnClickListener {
 
     private var _binding: FragmentDiceBinding? = null
 
     private val binding get() = _binding!!
-    private var dado: Dice = Dice()
+    private lateinit var dice: Dice
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
 
         _binding = FragmentDiceBinding.inflate(inflater, container, false)
         val root: View = binding.root
@@ -37,6 +31,8 @@ class DiceFragment : Fragment(), View.OnClickListener {
         binding.btnD6.setOnClickListener(this)
         binding.btnD4.setOnClickListener(this)
 
+        dice = Dice(binding.txtNum, binding.imgDice, requireContext())
+
         return root
     }
 
@@ -47,51 +43,28 @@ class DiceFragment : Fragment(), View.OnClickListener {
 
     override fun onClick(p0: View?) {
         if (p0 != null) {
-            var resource: Int = 0
+            var type = 20
             when (p0.id) {
                 binding.btnD20.id -> {
-                    dado.tipo = "D20"
-                    resource = R.drawable.ic_d20
+                    type = 20
                 }
                 binding.btnD12.id -> {
-                    dado.tipo = "D12"
-                    resource = R.drawable.ic_d12
+                    type = 12
                 }
                 binding.btnD10.id -> {
-                    dado.tipo = "D10"
-                    resource = R.drawable.ic_d10
+                    type = 10
                 }
                 binding.btnD8.id -> {
-                    dado.tipo = "D8"
-                    resource = R.drawable.ic_d8
+                    type = 8
                 }
                 binding.btnD6.id -> {
-                    dado.tipo = "D6"
-                    resource = R.drawable.ic_d6
+                    type = 6
                 }
                 binding.btnD4.id -> {
-                    dado.tipo = "D4"
-                    resource = R.drawable.ic_d4
+                    type = 4
                 }
             }
-            Log.i("Image", "Image changed to " + resources.getResourceName(resource))
-            if (resource != 0) {
-                binding.imgDice.setImageResource(resource)
-                var count = 0
-                val end = Random.nextInt(10, 30)
-                Timer().scheduleAtFixedRate(0, 100) {
-                    activity?.runOnUiThread {
-                        val num =
-                            Random.nextInt(1, dado.tipo.removePrefix("D").toInt() + 1).toString()
-                        binding.txtNum.text = num
-                        Log.d("Timer", num)
-                    }
-                    if (++count >= end) cancel()
-                }
-                //dado.lanzarDado()
-            } else {
-                Toast.makeText(context, "Error", Toast.LENGTH_SHORT).show()
-            }
+            dice.diceClicked(type)
         }
     }
 
